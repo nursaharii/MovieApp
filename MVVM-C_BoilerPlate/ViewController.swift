93607c09c47b05aa3d62391b3d298b7167387c25
Nurşah Ari <nursahari@hotmail.com>
2024-02-17 12:55:58 +0300

    Initial Commit

---
+++ MVVM-C_BoilerPlate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-C_BoilerPlate
+//
+//  Created by Nurşah Ari on 17.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
